d16b0ff0bb5d5144b20f228c001b54151a3c9504
foozie3moons <matthew.bell.290@gmail.com>
2017-09-01 10:13:53 -0700

    added basic structure and backgrounds. added basic timer

--- bomb.js
+++ bomb.js
@@ -1,6 +1,21 @@
 console.log("javascript running");
 
+function startTime() {
+  var h = 0;
+  var m = 0;
+  var s = 30;
+  m = checkTime(m);
+  s = checkTime(s);
+  document.querySelector('.timer').innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   console.log("DOM loaded");
+  startTime();
 });
-
